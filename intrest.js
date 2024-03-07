88763bc688077211ef1b940d6d9938d242ebbeff
pavankumar187 <pavankumarkodakalla187@gmail.com>
2024-03-07 12:47:17 +0530

    commit

--- intrest.js
+++ intrest.js
@@ -1,14 +1,14 @@
-function Getamount()
+function getAmount()
 {
-    let p,t,r,si,amt;
-    p=pamt.value;
-    t=time.value;
-    r=roi.value;
-    si=p*t*r/100;
-    amt=+p+si
-    intrest.value=`intrestamount :`+si;
-    totamt.value=`Totalamount :`+si;
-    document.write(`totamt.value`);
-    prompt(`valid time`);
-
+	let p, t, r, si, amt;	//local vars
+	//data coll
+	p=pamt.value;
+	t=time.value;
+	r=roi.value;
+	//calc's
+	si=p*t*r/100;
+	amt=+p+si;
+	//displaying results
+	interest.value='Interest Amount	:'+si;
+	totAmt.value='Total Amount	:'+amt;
 }
